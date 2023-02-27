abd3815b6c2d5ffef54119b5ef11c57577dcb619
Quentin Santos <qsantos@qsantos.fr>
2023-02-27 19:56:53 +0100

    Add memoized version

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use memoize::memoize;
 use num_bigint::BigInt;
 
 pub fn naive(n: u64) -> BigInt {
@@ -5,5 +6,14 @@ pub fn naive(n: u64) -> BigInt {
         0 => BigInt::from(0),
         1 => BigInt::from(1),
         n => naive(n - 1) + naive(n - 2),
+    }
+}
+
+#[memoize]
+pub fn memoized(n: u64) -> BigInt {
+    match n {
+        0 => BigInt::from(0),
+        1 => BigInt::from(1),
+        n => memoized(n - 1) + memoized(n - 2),
     }
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,4 +1,4 @@
-use fibo::naive;
+use fibo::{memoized, naive};
 
 use num_bigint::BigInt;
 
@@ -13,4 +13,27 @@ fn test_naive() {
     assert_eq!(naive(6), BigInt::from(8));
     assert_eq!(naive(7), BigInt::from(13));
     assert_eq!(naive(30), BigInt::from(832040));
+}
+
+#[test]
+fn test_memoized() {
+    assert_eq!(memoized(0), BigInt::from(0));
+    assert_eq!(memoized(1), BigInt::from(1));
+    assert_eq!(memoized(2), BigInt::from(1));
+    assert_eq!(memoized(3), BigInt::from(2));
+    assert_eq!(memoized(4), BigInt::from(3));
+    assert_eq!(memoized(5), BigInt::from(5));
+    assert_eq!(memoized(6), BigInt::from(8));
+    assert_eq!(memoized(7), BigInt::from(13));
+    assert_eq!(memoized(30), BigInt::from(832040));
+    assert_eq!(
+        memoized(1000),
+        BigInt::parse_bytes(
+            b"434665576869374564356885276750406258025646605173717804024817290895365\
+            55417949051890403879840079255169295922593080322634775209689623239873322\
+            471161642996440906533187938298969649928516003704476137795166849228875",
+            10,
+        )
+        .unwrap(),
+    );
 }
